9b3b1dec4281eb6ec107f87697cf99ea3c9e9daf
ccaudril <44361337+ccaudril@users.noreply.github.com>
2020-11-06 00:35:54 +0100

    Add files via upload

---
+++ Doxygen/hierarchy.js
@@ -0,0 +1,50 @@
+var hierarchy =
+[
+    [ "Composite", "class_composite.html", [
+      [ "CompteRacine", "class_compte_racine.html", null ],
+      [ "CompteVirtuel", "class_compte_virtuel.html", null ]
+    ] ],
+    [ "Compte", "class_compte.html", [
+      [ "CompteFeuille", "class_compte_feuille.html", [
+        [ "Deficit", "class_deficit.html", null ],
+        [ "Excedent", "class_excedent.html", null ],
+        [ "Resultat", "class_resultat.html", null ]
+      ] ],
+      [ "CompteVirtuel", "class_compte_virtuel.html", null ]
+    ] ],
+    [ "CompteFactory", "class_compte_factory.html", null ],
+    [ "CompteStrategy", "class_compte_strategy.html", [
+      [ "StrategyAD", "class_strategy_a_d.html", null ],
+      [ "StrategyPR", "class_strategy_p_r.html", null ]
+    ] ],
+    [ "TransactionManager::ConstIterator", "class_transaction_manager_1_1_const_iterator.html", null ],
+    [ "Composite::deepIterator", "class_composite_1_1deep_iterator.html", null ],
+    [ "TransactionManager::FilterIterator", "class_transaction_manager_1_1_filter_iterator.html", null ],
+    [ "CompteRacine::Handler", "struct_compte_racine_1_1_handler.html", null ],
+    [ "TransactionManager::Handler", "struct_transaction_manager_1_1_handler.html", null ],
+    [ "iterator", null, [
+      [ "Composite::iterator", "class_composite_1_1iterator.html", null ]
+    ] ],
+    [ "TransactionManager::Iterator", "class_transaction_manager_1_1_iterator.html", null ],
+    [ "Operations", "class_operations.html", null ],
+    [ "QComboBox", null, [
+      [ "ChoixCompte", "class_choix_compte.html", null ]
+    ] ],
+    [ "QDialog", null, [
+      [ "creerCompteDialog", "classcreer_compte_dialog.html", null ],
+      [ "fenetreRapprochement", "classfenetre_rapprochement.html", null ]
+    ] ],
+    [ "QMainWindow", null, [
+      [ "MainWindow", "class_main_window.html", null ]
+    ] ],
+    [ "QTableWidgetItem", null, [
+      [ "TransactionItem", "class_transaction_item.html", null ]
+    ] ],
+    [ "QTreeWidgetItem", null, [
+      [ "CompteItem", "class_compte_item.html", null ]
+    ] ],
+    [ "SauvegardeXML", "class_sauvegarde_x_m_l.html", null ],
+    [ "Transaction", "class_transaction.html", null ],
+    [ "TransactionManager", "class_transaction_manager.html", null ],
+    [ "TripletTransaction", "class_triplet_transaction.html", null ]
+];
